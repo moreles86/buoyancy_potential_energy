584bbacfe04d475e6c7e203c312fb4aab0059995
Efrain Moreles <moreles@atmosfera.unam.mx>
2026-05-13 15:18:17 -0700

    Update buoyancy_potential_energy.m

--- scripts/matlab/buoyancy_potential_energy.m
+++ scripts/matlab/buoyancy_potential_energy.m
@@ -1,47 +1,57 @@
-% Function to calculate the BUOYANCY POTENTIAL ENERGY (BPE)
-% BPE quantifies the buoyancy potential energy required for a water parcel in hydrostatic equilibrium to be vertically displaced from a specific reference height.
+function [BPE_mass, BPE_volume, p] = buoyancy_potential_energy(p, SA, CT, Peq)
+% Buoyancy Potential Energy (BPE) from accurate energetic formulations
+% BPE quantifies the buoyancy potential energy required for a water parcel in hydrostatic equilibrium to be vertically displaced from a specific reference height zint.
 
-function [BPE,z] = buoyancy_potential_energy(rho, z, zint)
-% Parameters:
-%   - Depth (z) and density (rho) data should go from the deepest to the shallowest and be column vectors
-%   - rho: Sigma-0 Potential Density Anomaly profile (kg*m^-3)
-%   - z: Heights (m) with negative units. The z-vector can be non-equidistant.
-%   - zint: Reference height (m)
-% Return:
-%   - BPE: Buoyancy potential energy profile (J*m^-3)
-%   - z: Heights (m)
+% Parameters
+% ----------
+% p: Sea pressure (dbar)
+% SA: Absolute Salinity (g kg-1)
+% CT: Conservative Temperature (°C)
+% Peq: Pressure of the equilibrium position of the parcel (dbar)
 
-% If there is no data at zint, it is interpolated
-if sum(z(z == zint), 'omitnan') == 0
-    f = @(x) interp1(z, rho, x, 'linear');
-    it = find(z >= zint, 1);
-    z = [z(1:it-1); zint; z(it:end)];
-    rho = [rho(1:it-1); f(zint); rho(it:end)];
+% Returns
+% ----------
+% BPE_mass: BPE per unit mass (J kg-1)
+% BPE_volume: BPE per unit volume (J m-3)
+% p: Sea pressure (dbar)
+
+% Notes
+% ----------
+% TEOS-10 implementation for BPE follows https://doi.org/10.5194/egusphere-2025-3359-RC1
+
+% If there is no data at Peq, it is interpolated in SA and CT
+if sum(find(p==Peq),'omitnan') == 0
+    p_ext=[Peq; p]; p_ext=sort(p_ext,'ascend');
+
+    % Interpolate SA and CT at Peq
+    SA_ext= interp1(p,SA,p_ext,'linear','extrap');
+    CT_ext= interp1(p,CT,p_ext,'linear','extrap');
+
+    % Rename variables
+    p=p_ext;
+    SA=SA_ext;
+    CT=CT_ext;
 end
 
-g = 9.81; % Acceleration of gravity
-nz = length(rho); % Amount of data in the vertical
-
-it = find(z == zint, 1); 
-dz = diff(z); 
-rho_int = rho(it); % Density at the reference height zint
+iPt=find(p==Peq);
 
 % Computation of BPE
-S = NaN(nz, 1);
-S(it) = 0;
+BPE_mass=NaN(length(p),1);
+BPE_volume=NaN(length(p),1);
+specvol=NaN(length(p),1); % Specific volume (m3 kg-1)
 
-for i = 1:1:it-1
-    sr = rho(i:it-1) + rho(i+1:it);
-    sdz = dz(i:it-1);
-    S(i) = -0.5*sum(sr.*sdz);
-end
-for i = it+1:1:nz
-    sr = rho(it:i-1) + rho(it+1:i);
-    sdz = dz(it:i-1);
-    S(i) = 0.5*sum(sr.*sdz);
-end
+for k=1:length(p)
+    iP2=k;
+    e1 = gsw_enthalpy(SA(iPt),CT(iPt),p(iP2)); % Specific enthalpy [J/kg]
+    e2 = gsw_enthalpy(SA(iPt),CT(iPt),p(iPt));
+    gsdh = gsw_geo_strf_dyn_height(SA,CT,p,p(iPt));
+    e3 = gsw_enthalpy(35.16504,0,p(iP2));
+    e4 = gsw_enthalpy(35.16504,0,p(iPt));
+    specvol(k) = gsw_specvol(SA(iPt),CT(iPt),p(iP2));
 
-BPE = (g*(z-zint)*rho_int)-(g*S);
+    BPE_mass(k)=(e1-e2+gsdh(iP2)-e3+e4);
+    BPE_volume(k)=BPE_mass(k)/specvol(k);
+end
 
 end
 
